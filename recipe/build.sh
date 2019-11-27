70008e6178ff59c9f28b1ea7d52cdca7bd2bcdd7
Anthony Scopatz <scopatz@gmail.com>
2019-11-27 16:05:32 -0500

    TIME_WITH_SYS_TIME 1

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 set -ex
 
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig:${PKG_CONFIG_PATH}"
-export CFLAGS="${CFLAGS} -I ${PREFIX}/include/freetype2 -I ${PREFIX}/include/harfbuzz -D_POSIX_C_SOURCE=199309L"
+export CFLAGS="${CFLAGS} -I ${PREFIX}/include/freetype2 -I ${PREFIX}/include/harfbuzz -DTIME_WITH_SYS_TIME=1"
 $PYTHON setup.py linux-package --prefix=$PREFIX --update-check-interval=0
 
 # add bash completions

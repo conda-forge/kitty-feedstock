30a711c656921d3b2c9b67d47450df8015afb5d8
Anthony Scopatz <scopatz@gmail.com>
2019-11-27 15:54:17 -0500

    add additional CFLAG

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 set -ex
 
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig:${PKG_CONFIG_PATH}"
-export CFLAGS="${CFLAGS} -I ${PREFIX}/include/freetype2 -I ${PREFIX}/include/harfbuzz"
+export CFLAGS="${CFLAGS} -I ${PREFIX}/include/freetype2 -I ${PREFIX}/include/harfbuzz -D_POSIX_C_SOURCE=199309L"
 $PYTHON setup.py linux-package --prefix=$PREFIX --update-check-interval=0
 
 # add bash completions
